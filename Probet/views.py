5db661d6182b9be2ad6dd9fe9414d291b56f43c2
Emre Sülün <emresulun93@gmail.com>
2018-12-22 15:35:46 +0300

    Sign up completed

--- Probet/views.py
+++ Probet/views.py
@@ -4,97 +4,112 @@ import sqlite3
 
 
 def index(request):
-    print(dir(request))
-    if request.method == "POST":
-        post = request.POST
-        tckn = post["tckn"]
-        password = post["password"]
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
-        parameters = [tckn, password]
-        cursor.execute("SELECT customer_id FROM Customer WHERE customer_id=? AND password=?", parameters)
-        auth = cursor.fetchone()
-        if auth is not None:
-            request.session.tckn = tckn
-            return render(request, "frontend/profile.html")
-        else:
-            return render(request, "frontend/index.html")
+	print(dir(request))
+	if request.method == "POST":
+		post = request.POST
+		tckn = post["tckn"]
+		password = post["password"]
+		connection = sqlite3.connect('db.sqlite3')
+		cursor = connection.cursor()
+		parameters = [tckn, password]
+		cursor.execute("SELECT customer_id FROM Customer WHERE customer_id=? AND password=?", parameters)
+		auth = cursor.fetchone()
+		if auth is not None:
+			request.session.tckn = tckn
+			return render(request, "frontend/profile.html")
+		else:
+			return render(request, "frontend/index.html")
 
+	else:
+		# Get all teams from db
+		connection = sqlite3.connect('db.sqlite3')
+		cursor = connection.cursor()
+		cursor.execute("SELECT customer_id, first_name, pmessage FROM Post NATURAL JOIN Customer")
+		postList = cursor.fetchall()
 
-    else:
-        # Get all teams from db
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
-        cursor.execute("SELECT customer_id, first_name, pmessage FROM Post NATURAL JOIN Customer")
-        postList = cursor.fetchall()
+		cursor2 = connection.cursor()
+		cursor2.execute(
+			"SELECT H.name, A.name, odd_amount FROM Odd o NATURAL JOIN Game JOIN Team H JOIN Team A Where H.team_id  = home_team_id AND A.team_id = away_team_id AND EXISTS (SELECT odd_amount  FROM Odd o1 WHERE o.odd_type = o1.odd_type AND odd_type = 'MS0' AND o.odd_amount IS NOT NULL)")
+		gameList = cursor2.fetchall()
 
-        cursor2 = connection.cursor()
-        cursor2.execute(
-            "SELECT H.name, A.name, odd_amount FROM Odd o NATURAL JOIN Game JOIN Team H JOIN Team A Where H.team_id  = home_team_id AND A.team_id = away_team_id AND EXISTS (SELECT odd_amount  FROM Odd o1 WHERE o.odd_type = o1.odd_type AND odd_type = 'MS0' AND o.odd_amount IS NOT NULL)")
-        gameList = cursor2.fetchall()
+		context = {
+			"postList": postList,
+			"gameList": gameList
+		}
 
-        context = {
-            "postList": postList,
-            "gameList": gameList
-        }
-
-        connection.close()
-        return render(request, 'frontend/index.html', context)
+		connection.close()
+		return render(request, 'frontend/index.html', context)
 
 
 def signup(request):
-    return render(request, "frontend/signup.html")
+	if request.method == "POST":  # User registration
+		connection = sqlite3.connect('db.sqlite3')
+		cursor = connection.cursor()
+
+		post = request.POST
+		parameters = [post['tckn'], post['name'], post['lastname'], post['email'], post['pass'], post['birthdate']]
+
+		cursor.execute("INSERT INTO Customer VALUES (?, NULL, NULL, NULL, ?, ?, ?, ?, NULL, 0, ?)", parameters)
+
+		connection.commit()
+		connection.close()
+		return HttpResponse("congrats, you are signed up")
+
+	else:  # GET request
+		return render(request, "frontend/signup.html")
 
 
 def teams(request):
-    # Get all teams from db
-    teamId = request.GET["id"]
-    connection = sqlite3.connect('db.sqlite3')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Team WHERE team_id=?", teamId)
-    team = cursor.fetchone()
+	# Get all teams from db
+	teamId = request.GET["id"]
+	connection = sqlite3.connect('db.sqlite3')
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM Team WHERE team_id=?", teamId)
+	team = cursor.fetchone()
 
-    if team is None:
-        return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
+	if team is None:
+		return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
 
-    context = {
-        "team": team
-    }
+	context = {
+		"team": team
+	}
 
-    connection.commit()  # Required for updating things
-    connection.close()
-    return render(request, "frontend/team.html", context)
+	connection.commit()  # Required for updating things
+	connection.close()
+	return render(request, "frontend/team.html", context)
 
 
 def customers(request):
-    userId = request.GET["id"]
-    connection = sqlite3.connect('db.sqlite3')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Customer WHERE customer_id=?", userId)
-    customer = cursor.fetchone()
-#ID düzeltilecek hep 1 veriyoz, front end kısmında kupon id, ve status
-    cursor.execute("SELECT home.name, away.name, odd_type,odd_amount FROM Odd NATURAL JOIN Game INNER JOIN Team home ON home.team_id=home_team_id INNER JOIN Team away ON away.team_id=away_team_id NATURAL JOIN INCLUDES NATURAL JOIN BetSlip WHERE customer_id = 1 AND status = 'waiting' ")
-    get_current_slip = cursor.fetchall()
+	userId = request.GET["id"]
+	connection = sqlite3.connect('db.sqlite3')
+	cursor = connection.cursor()
+	cursor.execute("SELECT * FROM Customer WHERE customer_id=?", userId)
+	customer = cursor.fetchone()
+	# ID düzeltilecek hep 1 veriyoz, front end kısmında kupon id, ve status
+	cursor.execute(
+		"SELECT home.name, away.name, odd_type,odd_amount FROM Odd NATURAL JOIN Game INNER JOIN Team home ON home.team_id=home_team_id INNER JOIN Team away ON away.team_id=away_team_id NATURAL JOIN INCLUDES NATURAL JOIN BetSlip WHERE customer_id = 1 AND status = 'waiting' ")
+	get_current_slip = cursor.fetchall()
 
-    cursor.execute("SELECT home.name, away.name, odd_type, odd_amount FROM Odd NATURAL JOIN Game INNER JOIN Team home ON home.team_id=home_team_id INNER JOIN Team away ON away.team_id=away_team_id NATURAL JOIN INCLUDES NATURAL JOIN BetSlip WHERE customer_id = 1 AND status != 'waiting' ")
-    get_old_slip = cursor.fetchall()
-    if customer is None:
-        return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
+	cursor.execute(
+		"SELECT home.name, away.name, odd_type, odd_amount FROM Odd NATURAL JOIN Game INNER JOIN Team home ON home.team_id=home_team_id INNER JOIN Team away ON away.team_id=away_team_id NATURAL JOIN INCLUDES NATURAL JOIN BetSlip WHERE customer_id = 1 AND status != 'waiting' ")
+	get_old_slip = cursor.fetchall()
+	if customer is None:
+		return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
 
-    context = {
-        "customer": customer,
-        "current_slip": get_current_slip,
-        "old_slip": get_old_slip
-    }
+	context = {
+		"customer": customer,
+		"current_slip": get_current_slip,
+		"old_slip": get_old_slip
+	}
 
-    connection.commit()  # Required for updating things
-    connection.close()
-    return render(request, "frontend/profile.html", context)
+	connection.commit()  # Required for updating things
+	connection.close()
+	return render(request, "frontend/profile.html", context)
 
 
 def sa(request):
-    return HttpResponse("as")
+	return HttpResponse("as")
 
 
 def pdfReports(request):
-    return HttpResponseRedirect("http://emre.sulun.ug.bilkent.edu.tr/cs353")
+	return HttpResponseRedirect("http://emre.sulun.ug.bilkent.edu.tr/cs353")
