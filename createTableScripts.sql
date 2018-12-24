d51ecf2cdce0512a8902ca420bbcc36a1189f8d6
Eray <sahin.eray@outlook.com.tr>
2018-12-24 21:51:44 +0300

    Updates to db

--- createTableScripts.sql
+++ createTableScripts.sql
@@ -12,185 +12,177 @@ rank INT DEFAULT 0,
 birth_date DATE, 
 PRIMARY KEY (customer_id));
 
-drop table Receives;
-drop table Message;
-
 CREATE TABLE Message ( 
-message_id INT NOT NULL,
+message_id INTEGER PRIMARY KEY AUTOINCREMENT,
 message_text VARCHAR(255) NOT NULL, 
 date  DATE,
 customer_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (message_id));
-
-CREATE TABLE Receives ( 
-message_id INT NOT NULL, 
-customer_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
-FOREIGN KEY(message_id) REFERENCES Message(message_id), 
-PRIMARY KEY (message_id, customer_id));
-
-CREATE TABLE Post ( 
-post_id INT NOT NULL, 
-date  DATE NOT NULL, 
-pmessage VARCHAR(255), 
-customer_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (post_id));
-
-CREATE TABLE Post_like ( 
-post_id INT NOT NULL, 
-customer_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-FOREIGN KEY(post_id) REFERENCES Post(post_id), 
-PRIMARY KEY (post_id, customer_id));
-
-CREATE TABLE Flags ( 
-post_id INT NOT NULL, 
-customer_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-FOREIGN KEY(post_id) REFERENCES Post(post_id), 
-PRIMARY KEY (post_id, customer_id));
-
-drop table Comment;
-CREATE TABLE Comment ( 
-post_id INT NOT NULL, 
-c_id INT NOT NULL,
-c_message   VARCHAR(255) NOT NULL, 
-date DATE NOT NULL, 
-customer_id INT NOT NULL, 
-FOREIGN KEY(post_id) REFERENCES Post(post_id), 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (post_id, c_id));
-
-CREATE TABLE Comment_like ( 
-customer_id INT NOT NULL, 
-post_id   INT NOT NULL, 
-c_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-FOREIGN KEY(c_id) REFERENCES Comment(c_id), 
-FOREIGN KEY(post_id) REFERENCES Post(post_id), 
-PRIMARY KEY (customer_id, post_id, c_id));
-
-CREATE TABLE Follows ( 
-customer_id INT NOT NULL, 
-customer2_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-FOREIGN KEY(customer2_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (customer_id, customer2_id));
-
-CREATE TABLE Blocks ( 
-customer_id INT NOT NULL, 
-customer2_id INT NOT NULL, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-FOREIGN KEY(customer2_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (customer_id, customer2_id));
-
-CREATE TABLE Wallet ( 
-customer_id INT NOT NULL, 
-wallet_id INT NOT NULL, 
-balance DOUBLE, 
-payment_method VARCHAR(10), 
-card_number CHAR(16), 
-cvc CHAR(3),
-expire_date DATE, 
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id), 
-PRIMARY KEY (customer_id, wallet_id));
-
-CREATE TABLE BetSlip ( 
-bet_slip_id INT NOT NULL, 
-customer_id INT NOT NULL, 
-bet_amount DOUBLE NOT NULL, 
-num_of_games  INT NOT NULL,
-date_created DATE NOT NULL, 
-status VARCHAR(10) NOT NULL, 
-PRIMARY KEY (bet_slip_id, customer_id), 
 FOREIGN KEY(customer_id) REFERENCES Customer(customer_id));
 
-CREATE TABLE Game ( 
-game_id  INT NOT NULL, 
+CREATE TABLE Receives (
+message_id INT NOT NULL,
+customer_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(message_id) REFERENCES Message(message_id),
+PRIMARY KEY (message_id, customer_id));
+
+CREATE TABLE Follows (
+customer_id INT NOT NULL,
+customer2_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(customer2_id) REFERENCES Customer(customer_id),
+PRIMARY KEY (customer_id, customer2_id));
+
+CREATE TABLE Blocks (
+customer_id INT NOT NULL,
+customer2_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(customer2_id) REFERENCES Customer(customer_id),
+PRIMARY KEY (customer_id, customer2_id));
+
+CREATE TABLE Team (
+team_id INTEGER PRIMARY KEY AUTOINCREMENT,
+name VARCHAR(20) NOT NULL,
+league VARCHAR(20),
+place_in_league INT,
+country VARCHAR(20),
+team_pic VARCHAR(255));
+
+CREATE TABLE Game (
+game_id INTEGER PRIMARY KEY AUTOINCREMENT,
 date DATE,
-home_score INT, 
-away_score INT, 
-min_bet_number INT, 
-start_time DATE, 
-end_time DATE, 
-home_team_id INT NOT NULL, 
-away_team_id INT NOT NULL, 
-PRIMARY KEY (game_id), 
-FOREIGN KEY(home_team_id) REFERENCES Team(team_id), 
+home_score INT,
+away_score INT,
+min_bet_number INT,
+start_time DATE,
+end_time DATE,
+home_team_id INT NOT NULL,
+away_team_id INT NOT NULL,
+FOREIGN KEY(home_team_id) REFERENCES Team(team_id),
 FOREIGN KEY(away_team_id) REFERENCES Team(team_id));
 
-CREATE TABLE Editor ( 
-editor_id INT NOT NULL, 
-editor_first_name VARCHAR(255) NOT NULL, 
-editor_last_name VARCHAR(255) NOT NULL, 
-PRIMARY KEY (editor_id));
+CREATE TABLE Odd (
+game_id INT NOT NULL,
+odd_type VARCHAR(10),
+odd_amount DOUBLE,
+PRIMARY KEY (game_id, odd_type),
+FOREIGN KEY(game_id) REFERENCES Game(game_id));
 
-drop table Suggestion;
-CREATE TABLE Suggestion ( 
+CREATE TABLE Editor (
+editor_id INT NOT NULL,
+PRIMARY KEY (editor_id)
+FOREIGN KEY (editor_id) REFERENCES Customer(customer_id));
+
+CREATE TABLE Suggestion (
+sug_id INTEGER PRIMARY KEY AUTOINCREMENT,
+text VARCHAR(255),
+date DATE);
+
+CREATE TABLE Suggests (
+game_id INT NOT NULL,
 sug_id INT NOT NULL,
-text VARCHAR(255), 
-date DATE, 
-PRIMARY KEY (sug_id));
-
-CREATE TABLE Suggests ( 
-game_id INT NOT NULL, 
-sug_id INT NOT NULL, 
-editor_id INT NOT NULL, 
-PRIMARY KEY (game_id, sug_id, editor_id), 
-FOREIGN KEY(sug_id) REFERENCES Suggestion(sug_id), 
-FOREIGN KEY(editor_id) REFERENCES Editor(editor_id), 
+editor_id INT NOT NULL,
+PRIMARY KEY (game_id, sug_id, editor_id),
+FOREIGN KEY(sug_id) REFERENCES Suggestion(sug_id),
+FOREIGN KEY(editor_id) REFERENCES Editor(editor_id),
 FOREIGN KEY(game_id) REFERENCES Game(game_id));
 
-CREATE TABLE Odd ( 
-game_id INT NOT NULL, 
-odd_type VARCHAR(10), 
-odd_amount DOUBLE, 
-PRIMARY KEY (game_id, odd_type), 
-FOREIGN KEY(game_id) REFERENCES Game(game_id));
+CREATE TABLE Statistics (
+sta_id INTEGER PRIMARY KEY AUTOINCREMENT,
+game_id INT NOT NULL,
+editor_id INT NOT NULL,
+FOREIGN KEY(game_id) REFERENCES Game(game_id),
+FOREIGN KEY(editor_id) REFERENCES Editor(editor_id));
 
-CREATE TABLE Includes ( 
-game_id INT NOT NULL, 
-odd_type VARCHAR(10), 
-customer_id INT NOT NULL, 
-bet_slip_id INT NOT NULL, 
-PRIMARY KEY (game_id, odd_type, customer_id, bet_slip_id), 
-FOREIGN KEY(game_id, odd_type) REFERENCES Odd, 
-FOREIGN KEY(customer_id, bet_slip_id) REFERENCES BetSlip);
-
-CREATE TABLE Statistics ( 
-sta_id INT NOT NULL, 
-game_id INT NOT NULL, 
-editor_id INT NOT NULL, 
-FOREIGN KEY(game_id) REFERENCES Game(game_id), 
-FOREIGN KEY(editor_id) REFERENCES Editor(editor_id), 
-PRIMARY KEY (sta_id));
-
-CREATE TABLE PreStat ( 
-sta_id INT NOT NULL, 
-num_of_suspended_players INT, 
-num_of_suspended_injuries INT, 
-top_scorer VARCHAR(20), 
+CREATE TABLE PreStat (
+sta_id INT NOT NULL,
+num_of_suspended_players INT,
+num_of_suspended_injuries INT,
+top_scorer VARCHAR(20),
 shots_per_game INT,
-FOREIGN KEY(sta_id) REFERENCES Statistics, 
+FOREIGN KEY(sta_id) REFERENCES Statistics,
 PRIMARY KEY (sta_id));
 
-CREATE TABLE PostStat ( 
-sta_id INT NOT NULL, 
-possession INT, 
-dribbles INT, 
-fouls INT, 
-pass_success INT, 
+CREATE TABLE PostStat (
+sta_id INT NOT NULL,
+possession INT,
+dribbles INT,
+fouls INT,
+pass_success INT,
 tackles INT,
-FOREIGN KEY(sta_id) REFERENCES Statistics, 
+FOREIGN KEY(sta_id) REFERENCES Statistics,
 PRIMARY KEY (sta_id));
 
-CREATE TABLE Team ( 
-team_id INT NOT NULL, 
-name VARCHAR(20) NOT NULL, 
-league VARCHAR(20), 
-place_in_league INT, 
-country VARCHAR(20),
-team_pic VARCHAR(255),
-PRIMARY KEY (team_id));
+CREATE TABLE Post (
+post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+date  DATE NOT NULL,
+pmessage VARCHAR(255),
+customer_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id));
+
+CREATE TABLE Post_like (
+post_id INT NOT NULL,
+customer_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(post_id) REFERENCES Post(post_id),
+PRIMARY KEY (post_id, customer_id));
+
+CREATE TABLE Flags (
+post_id INT NOT NULL,
+customer_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(post_id) REFERENCES Post(post_id),
+PRIMARY KEY (post_id, customer_id));
+
+-- * We might need to change this *
+CREATE TABLE Wallet (
+wallet_id INTEGER PRIMARY KEY AUTOINCREMENT,
+customer_id INT NOT NULL,
+balance DOUBLE,
+payment_method VARCHAR(10),
+card_number CHAR(16),
+cvc CHAR(3),
+expire_date DATE,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+UNIQUE (customer_id, wallet_id));
+
+-- * We might need to change this *
+CREATE TABLE Comment (
+c_id INTEGER PRIMARY KEY AUTOINCREMENT,
+post_id INT NOT NULL,
+c_message   VARCHAR(255) NOT NULL,
+date DATE NOT NULL,
+customer_id INT NOT NULL,
+FOREIGN KEY(post_id) REFERENCES Post(post_id),
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+UNIQUE (post_id, c_id));
+
+CREATE TABLE Comment_like (
+customer_id INT NOT NULL,
+post_id   INT NOT NULL,
+c_id INT NOT NULL,
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
+FOREIGN KEY(c_id) REFERENCES Comment(c_id),
+FOREIGN KEY(post_id) REFERENCES Post(post_id),
+PRIMARY KEY (customer_id, post_id, c_id));
+
+
+-- * We might need to change this *
+CREATE TABLE BetSlip (
+bet_slip_id INTEGER PRIMARY KEY AUTOINCREMENT,
+customer_id INT NOT NULL,
+bet_amount DOUBLE NOT NULL,
+num_of_games  INT NOT NULL,
+date_created DATE NOT NULL,
+status VARCHAR(10) NOT NULL,
+UNIQUE (bet_slip_id, customer_id),
+FOREIGN KEY(customer_id) REFERENCES Customer(customer_id));
+
+CREATE TABLE Includes (
+game_id INT NOT NULL,
+odd_type VARCHAR(10),
+customer_id INT NOT NULL,
+bet_slip_id INT NOT NULL,
+PRIMARY KEY (game_id, odd_type, customer_id, bet_slip_id),
+FOREIGN KEY(game_id, odd_type) REFERENCES Odd,
+FOREIGN KEY(customer_id, bet_slip_id) REFERENCES BetSlip);
--- insertScripts.sql
+++ insertScripts.sql
@@ -1,126 +1,200 @@
 -- customer
 INSERT INTO Customer VALUES (353, 'Fenerbahçe', 5321234567, 'TR12345678901234567890', 'Özgür', 'Ulusoy',
             'ozgur.ulusoy@ug.bilkent.edu.tr', 'sifre', 'ozgur.jpg', 7, 80668800);
+
 INSERT INTO Customer VALUES (10042167494, 'Arsenal', 5548964444, 'TR09876543210987654321', 'Eray', 'Şahin',
             'eray.sahin@outlook.com.tr', 'benimsifre', 'eray.jpg', 1, 873273600);
 
-------------------------------------------------------------------------------------------------------------------------
+INSERT INTO Customer VALUES(12345678901, 'Atletico Madrid', 5421234567, 'TR11223344551122334455', 'İbrahim', 'Sezen',
+            'ibrahim.sezen@xmail.com', 'sezen1971', 'ibrahim.jpg', 1, 41418000);
 
--- message
-INSERT INTO Message VALUES(300, "Hello John! How are you?", 1544889600, 353);
-INSERT INTO Message VALUES(301, "Hi Ozgur. I'm fine...", 1544983200, 2);
-INSERT INTO Message VALUES(302, "Son mesaj", 1544889600, 353);
+INSERT INTO Customer VALUES(11111111111, 'Galatasaray', 5548002381, 'TR11111111112222222222', 'Yakup', 'Yazar',
+            'yakup_yazar_81@ymail.com', 'y1a2k3u4p5', 'yakup.jpg', 8, 360406800);
 
--- team
-INSERT INTO Team VALUES(3, 'Fenerbahçe', 'Süper Lig', 11, 'Turkey', '3.jpg');
-INSERT INTO Team VALUES(4, 'Beşiktaş', 'Süper Lig', 9, 'Turkey', '4.jpg');
-INSERT INTO Team VALUES(5, 'Manchester United', 'Premier League', 20, 'England', '5.jpg');
-
--- odd
-INSERT INTO Odd VALUES(80, 'MS0', 4.5);
-INSERT INTO Odd VALUES(80, 'MS1', 8.2);
-INSERT INTO Odd VALUES(80, 'MS2', 9.1);
-
-INSERT INTO Odd VALUES(81, 'MS0', 0.7);
-INSERT INTO Odd VALUES(81, 'MS1', 3.4);
-INSERT INTO Odd VALUES(81, 'MS2', 5.5);
-
-INSERT INTO Odd VALUES(82, 'MS0', 1.2);
-INSERT INTO Odd VALUES(82, 'MS1', 7.4);
-INSERT INTO Odd VALUES(82, 'MS2', 10.23);
-
-INSERT INTO Odd VALUES(83, 'MS0', 1.1);
-INSERT INTO Odd VALUES(83, 'MS1', 2.2);
-INSERT INTO Odd VALUES(83, 'MS2', 3.3);
-
--- game
-INSERT INTO Game VALUES(80, 1525197600, 3, 2, 4, 1525197600, 1525204800, 4, 3);
-INSERT INTO Game VALUES(81, 1534093200, 1, 1, 6, 1534093200, 1534100400, 1, 2);
-INSERT INTO Game VALUES(82, 1521547200, 1, 1, 6, 1521547200, 1521554400, 4, 5);
-INSERT INTO Game VALUES(83, 1546333200, NULL, NULL, 3, 1546333200, 1546344000, 1, 2);
-
--- bet slip
-INSERT INTO BetSlip VALUES(100, 1, 250.75, 2, 1522612800, 'lose');
-INSERT INTO BetSlip VALUES(101, 2, 12.5, 3, 1532804400, 'win');
-INSERT INTO BetSlip VALUES(102, 353, 50.5, 4, 1519999200, 'win');
-INSERT INTO BetSlip VALUES(103, 1, 20, 2, 1543665600, 'waiting');
-
-INSERT INTO BetSlip VALUES(104, 10042167494, 12, 1, 1522612800, 'lose');
-
--- includes
-INSERT INTO Includes VALUES(80, 'MS1', 1, 100);
-INSERT INTO Includes VALUES(82, 'MS2', 2, 101);
-INSERT INTO Includes VALUES(80, 'MS0', 353, 102);
-INSERT INTO Includes VALUES(83, 'MS1', 1, 103);
-
-INSERT INTO Includes VALUES(80, 'MS1', 10042167494, 104);
-
--- post
-INSERT INTO Post VALUES(40, 1541167200, "This is a post...", 1);
-INSERT INTO Post VALUES(41, 1542790800, "Another post...", 2);
-INSERT INTO Post VALUES(42, 1543136400, "A post...", 353);
 
 -- follows
 INSERT INTO Follows VALUES(1, 353);
 INSERT INTO Follows VALUES(353, 1);
 INSERT INTO Follows VALUES(2, 1);
 
+
 -- blocks
 INSERT INTO Blocks VALUES(353, 2);
 
--- post like
-INSERT INTO Post_like VALUES(40, 353);
-INSERT INTO Post_like VALUES(40, 2);
-INSERT INTO Post_like VALUES(41, 353);
 
--- flags
-INSERT INTO Flags VALUES(42, 2);
+-- message
+INSERT INTO Message (message_text, date, customer_id) VALUES("Hello John! How are you?", 1544889600, 353);
+INSERT INTO Message (message_text, date, customer_id) VALUES("Hi Ozgur. I'm fine...", 1544983200, 2);
+INSERT INTO Message (message_text, date, customer_id) VALUES("Son mesaj", 1544889600, 353);
 
--- wallet
-INSERT INTO Wallet VALUES(1, 21, 550.3, 'credit', '1111222233334444', '123', 1669896000);
-INSERT INTO Wallet VALUES(2, 22, 2000, 'credit', '9999888877776666', '987', 1641038400);
-INSERT INTO Wallet VALUES(353, 23, 180, 'credit', '6666555544443333', '654', 1648814400);
 
 -- receives
-INSERT INTO Receives VALUES(300, 2);
-INSERT INTO Receives VALUES(301, 353);
-INSERT INTO Receives VALUES(302, 2);
+INSERT INTO Receives VALUES(1, 2);
+INSERT INTO Receives VALUES(2, 353);
+INSERT INTO Receives VALUES(3, 2);
 
-CREATE TABLE Comment (
-post_id INT NOT NULL,
-c_id INT NOT NULL,
-c_message   VARCHAR(255) NOT NULL,
-date DATE NOT NULL,
-customer_id INT NOT NULL,
-FOREIGN KEY(post_id) REFERENCES Post(post_id),
-FOREIGN KEY(customer_id) REFERENCES Customer(customer_id),
-PRIMARY KEY (post_id, c_id));
 
--- comment
-INSERT INTO Comment VALUES(40, 700, "This is a comment", 1541188800, 353);
-INSERT INTO Comment VALUES(41, 701, "Another comment", 1542791700, 353);
-INSERT INTO Comment VALUES(42, 702, "A comment", 1543150860, 1);
+-- team
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Gençlerbirliği', 'TFF 1.Lig', 1, 'Turkey', '1.jpg');
 
--- comment like
-INSERT INTO Comment_like VALUES(1, 40, 700);
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Denizlispor', 'TFF 1.Lig', 2, 'Turkey', '2.jpg');
 
--- suggestion
-INSERT INTO Suggestion VALUES(800, "Banko maç... Gençlerbirliği alır.", 1544864400);
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Fenerbahçe', 'Süper Lig', 11, 'Turkey', '3.jpg');
+
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Beşiktaş', 'Süper Lig', 9, 'Turkey', '4.jpg');
+
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Manchester United', 'Premier League', 20, 'England', '5.jpg');
+
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Anadolu Efes', 'Tahincioğlu Basketbol Süper Lig', 3, 'Turkey', '6.jpg');
+
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Türk Telekom', 'Tahincioğlu Basketbol Süper Lig', 4, 'Turkey', '7.jpg');
+
+INSERT INTO Team (name, league, place_in_league, country, team_pic)
+            VALUES('Tofaş', 'Tahincioğlu Basketbol Süper Lig', 2, 'Turkey', '8.jpg');
+
+
+-- game
+
+-- (football games)
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1525197600, 3, 2, 4, 1525197600, 1525204800, 4, 3);
+
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1534093200, 1, 1, 6, 1534093200, 1534100400, 1, 2);
+
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1521547200, 1, 1, 6, 1521547200, 1521554400, 4, 5);
+
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1546333200, NULL, NULL, 3, 1546333200, 1546344000, 1, 2);
+
+-- (basketball games)
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1545555600, 81, 93, 5, 1545571800, 1545579000, 8, 6);
+
+INSERT INTO Game (date, home_score, away_score, min_bet_number, start_time, end_time, home_team_id, away_team_id)
+            VALUES(1546419600, NULL, NULL, 2, 1546443000, 1546450200, 7, 6);
+
+
+-- odd
+INSERT INTO Odd VALUES(1, 'MS0', 1.4);
+INSERT INTO Odd VALUES(1, 'MS1', 2.1);
+INSERT INTO Odd VALUES(1, 'MS2', 3.6);
+
+INSERT INTO Odd VALUES(2, 'MS0', 2.5);
+INSERT INTO Odd VALUES(2, 'MS1', 2.1);
+INSERT INTO Odd VALUES(2, 'MS2', 2.0);
+
+INSERT INTO Odd VALUES(3, 'MS0', 1.2);
+INSERT INTO Odd VALUES(3, 'MS1', 2.4);
+INSERT INTO Odd VALUES(3, 'MS2', 3.7);
+
+INSERT INTO Odd VALUES(4, 'MS0', 2.7);
+INSERT INTO Odd VALUES(4, 'MS1', 2.3);
+INSERT INTO Odd VALUES(4, 'MS2', 2.0);
+
+INSERT INTO Odd VALUES(4, 'MS0', 2.7);
+INSERT INTO Odd VALUES(4, 'MS1', 2.3);
+INSERT INTO Odd VALUES(4, 'MS2', 2.0);
+
+INSERT INTO Odd VALUES(5, 'MS1', 1.7);
+INSERT INTO Odd VALUES(5, 'MS2', 1.7);
+
+INSERT INTO Odd VALUES(6, 'MS1', 1.7);
+INSERT INTO Odd VALUES(6, 'MS2', 1.7);
+
 
 -- editor
-INSERT INTO Editor VALUES(90, 'Erman', 'Şafak');
-INSERT INTO Editor VALUES(91, 'Rıdvan', 'Yılmaz');
-INSERT INTO Editor VALUES(92, 'İbrahim', 'Sezen');
+INSERT INTO Editor VALUES(12345678901);
+INSERT INTO Editor VALUES(11111111111);
+
+
+-- suggestion
+INSERT INTO Suggestion (text, date) VALUES("Banko maç... Gençlerbirliği alır.", 1544864400);
+
 
 -- suggests
-INSERT INTO Suggests VALUES(83, 800, 90);
+INSERT INTO Suggests VALUES(4, 1, 12345678901);
+
 
 -- statistics
-INSERT INTO Statistics VALUES(400, 83, 92);
-INSERT INTO Statistics VALUES(401, 80, 91);
+INSERT INTO Statistics (game_id, editor_id) VALUES(5, 12345678901);
+INSERT INTO Statistics (game_id, editor_id) VALUES(1, 11111111111);
+
 
 -- pre statistics
-INSERT INTO PreStat VALUES(400, 5, 3, 'Mert Nobre', 4);
+INSERT INTO PreStat VALUES(1, 3, 1, 'Shane D.Larkin', 16);
+
 
 -- post statistics
-INSERT INTO PostStat VALUES(401, 63, 40, 3, 65, 7);
+INSERT INTO PostStat VALUES(2, 61, 40, 3, 47, 32);
+
+
+-- post
+INSERT INTO Post (date, pmessage, customer_id ) VALUES(1541167200, "This is a post...", 1);
+INSERT INTO Post (date, pmessage, customer_id ) VALUES(1542790800, "Another post...", 2);
+INSERT INTO Post (date, pmessage, customer_id ) VALUES(1543136400, "A post...", 353);
+
+
+-- post like
+INSERT INTO Post_like VALUES(1, 353);
+INSERT INTO Post_like VALUES(1, 2);
+INSERT INTO Post_like VALUES(2, 353);
+
+
+-- flags
+INSERT INTO Flags VALUES(3, 2);
+
+
+-- wallet *
+INSERT INTO Wallet (customer_id, balance, payment_method, card_number, cvc, expire_date)
+            VALUES(1, 550.3, 'credit', '1111222233334444', '123', 1669896000);
+
+INSERT INTO Wallet (customer_id, balance, payment_method, card_number, cvc, expire_date)
+            VALUES(2, 2000, 'credit', '9999888877776666', '987', 1641038400);
+
+INSERT INTO Wallet (customer_id, balance, payment_method, card_number, cvc, expire_date)
+            VALUES(353, 180, 'credit', '6666555544443333', '654', 1648814400);
+
+
+-- comment
+INSERT INTO Comment (post_id, c_message, date, customer_id) VALUES(1, "This is a comment", 1541188800, 353);
+INSERT INTO Comment (post_id, c_message, date, customer_id) VALUES(2, "Another comment", 1542791700, 353);
+INSERT INTO Comment (post_id, c_message, date, customer_id) VALUES(3, "A comment", 1543150860, 1);
+
+
+-- comment like
+INSERT INTO Comment_like VALUES(1, 1, 1);
+INSERT INTO Comment_like VALUES(6, 1, 1);
+
+
+-- bet slip
+INSERT INTO BetSlip (customer_id, bet_amount, num_of_games, date_created, status)
+            VALUES(1, 250.75, 2, 1522612800, 'lose');
+
+INSERT INTO BetSlip (customer_id, bet_amount, num_of_games, date_created, status)
+            VALUES(2, 12.5, 3, 1532804400, 'win');
+
+INSERT INTO BetSlip (customer_id, bet_amount, num_of_games, date_created, status)
+            VALUES(353, 50.5, 4, 1519999200, 'win');
+
+INSERT INTO BetSlip (customer_id, bet_amount, num_of_games, date_created, status)
+            VALUES(1, 20, 2, 1543665600, 'waiting');
+
+INSERT INTO BetSlip (customer_id, bet_amount, num_of_games, date_created, status)
+            VALUES(10042167494, 12, 1, 1522612800, 'lose');
+
+
+-- includes
+INSERT INTO Includes VALUES(80, 'MS1', 1, 100);
+INSERT INTO Includes VALUES(82, 'MS2', 2, 101);
+INSERT INTO Includes VALUES(80, 'MS0', 353, 102);
+INSERT INTO Includes VALUES(83, 'MS1', 1, 103);
+INSERT INTO Includes VALUES(80, 'MS1', 10042167494, 104);
