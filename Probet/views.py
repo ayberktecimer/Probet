4008c5b56ba4b795443072cb9a26f9e0a6a27794
Emre Sülün <emresulun93@gmail.com>
2019-01-02 19:50:16 +0300

    Show Average of scores played against teams which are in the first three places of the league (Report #1)

--- Probet/views.py
+++ Probet/views.py
@@ -81,8 +81,15 @@ def teams(request):
 		if team is None:
 			return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
 
+		cursor.execute(averageScoreSql(), [teamId, teamId, teamId])
+		try:
+			averageScore = round(cursor.fetchone()[0], 2)
+		except:
+			averageScore = 0
+
 		context = {
-			"team": team
+			"team": team,
+			"averageScore": averageScore
 		}
 
 		connection.commit()  # Required for updating things
@@ -345,3 +352,13 @@ def uninterestedLeaguesSql():
 		   "FROM (customerAndFriends NATURAL JOIN Includes NATURAL JOIN Game " \
 		   "INNER JOIN Team H ON H.team_id = game.home_team_id INNER JOIN Team A " \
 		   "ON A.team_id = Game.away_team_id)) SELECT DISTINCT league FROM Team WHERE league NOT IN playedLeagues"
+
+
+def averageScoreSql():
+	return "WITH firstThree AS (SELECT team_id FROM Team WHERE place_in_league < 4 AND league IN (SELECT league " \
+		   "FROM Team WHERE team_id = ?)), scoresAtHome AS (SELECT SUM(home_score) as homesum, COUNT(home_score) " \
+		   "as homecount FROM Game WHERE home_team_id = ? AND away_team_id IN firstThree), scoresAtAway " \
+		   "AS (SELECT SUM(away_score) as awaysum, COUNT(away_score) as awaycount FROM Game WHERE away_team_id = ? " \
+		   "AND home_team_id IN firstThree) SELECT (((SELECT homesum FROM scoresAtHome) + (SELECT awaysum " \
+		   "FROM scoresAtAway) * 1.0 / ((SELECT homecount FROM scoresAtHome) + (SELECT awaycount FROM scoresAtAway)))) " \
+		   "as average_score_against_first_three"
