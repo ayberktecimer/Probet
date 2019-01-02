94febfed4e297ef634e4c2c01158bbc0e1f10b39
Eray <sahin.eray@outlook.com.tr>
2019-01-03 00:22:31 +0300

    Repport bug fix

--- Probet/views.py
+++ Probet/views.py
@@ -81,7 +81,7 @@ def teams(request):
 		if team is None:
 			return HttpResponseRedirect("https://media.giphy.com/media/9SJazLPHLS8roFZMwZ/giphy.gif")
 
-		cursor.execute(averageScoreSql(), [teamId, teamId, teamId])
+		cursor.execute(averageScoreSql(), [teamId, teamId, teamId, teamId, teamId, teamId])
 		try:
 			averageScore = round(cursor.fetchone()[0], 2)
 		except:
@@ -360,13 +360,26 @@ def uninterestedLeaguesSql():
 
 
 def averageScoreSql():
-	return "WITH firstThree AS (SELECT team_id FROM Team WHERE place_in_league < 4 AND league IN (SELECT league " \
-		   "FROM Team WHERE team_id = ?)), scoresAtHome AS (SELECT SUM(home_score) as homesum, COUNT(home_score) " \
-		   "as homecount FROM Game WHERE home_team_id = ? AND away_team_id IN firstThree), scoresAtAway " \
-		   "AS (SELECT SUM(away_score) as awaysum, COUNT(away_score) as awaycount FROM Game WHERE away_team_id = ? " \
-		   "AND home_team_id IN firstThree) SELECT (((SELECT homesum FROM scoresAtHome) + (SELECT awaysum " \
-		   "FROM scoresAtAway) * 1.0 / ((SELECT homecount FROM scoresAtHome) + (SELECT awaycount FROM scoresAtAway)))) " \
-		   "as average_score_against_first_three"
+	return "WITH firstThree AS (SELECT team_id FROM Team WHERE place_in_league < 4 " \
+		   "                AND league IN (SELECT league FROM Team WHERE team_id = ?) " \
+		   "                AND NOT team_id = ?), " \
+		   "scoresAtHome AS (SELECT SUM(IFNULL(home_score, 0)) as homesum, " \
+		   "                COUNT(home_score) as homecount " \
+		   "                FROM Game " \
+		   "                WHERE home_team_id = ? " \
+		   "                AND away_team_id IN firstThree), " \
+		   "scoresAtAway AS (SELECT SUM(IFNULL(away_score, 0)) as awaysum, " \
+		   "                COUNT(away_score) as awaycount " \
+		   "                FROM Game " \
+		   "                WHERE away_team_id = ? " \
+		   "                AND home_team_id IN firstThree) " \
+		   "SELECT (((SELECT IFNULL(homesum, 0) FROM scoresAtHome) + " \
+		   "         (SELECT IFNULL(awaysum, 0) FROM scoresAtAway))) * 1.0 / " \
+		   "          (SELECT COUNT(game_id) " \
+		   "          FROM Game " \
+		   "          WHERE (home_team_id = ? " \
+		   "          OR away_team_id = ?) " \
+		   "          AND home_score IS NOT NULL) as average_score_against_first_three "
 
 
 def postlike(request):
@@ -388,6 +401,7 @@ def postlike(request):
 	connection.close()
 	return HttpResponse()
 
+
 def updateprofile(request):
 	if request.method == "GET":
 		return render(request, "frontend/updateProfile.html")
@@ -398,7 +412,6 @@ def updateprofile(request):
 		cursor.execute(
 			"SELECT  fav_team, phone_number, iban, email, password FROM Customer WHERE customer_id = ?;", [usr])
 
-
 		profile_data = cursor.fetchone()
 
 		fav_team = profile_data[0]
@@ -406,7 +419,6 @@ def updateprofile(request):
 		kban = profile_data[2]
 		email = profile_data[3]
 		password = profile_data[4]
-
 
 		if len(request.POST['favTeam']) != 0:
 			fav_team = request.POST['favTeam']
@@ -428,8 +440,9 @@ def updateprofile(request):
 		cursor.execute("SELECT name FROM TEAM")
 		teams = cursor.fetchall()
 
-
-		cursor.execute("UPDATE Customer SET fav_team = ?, phone_number = ?, iban = ?, email = ?, password = ? WHERE customer_id = ?",[fav_team, phone_number, kban, email, password, usr])
+		cursor.execute(
+			"UPDATE Customer SET fav_team = ?, phone_number = ?, iban = ?, email = ?, password = ? WHERE customer_id = ?",
+			[fav_team, phone_number, kban, email, password, usr])
 		connection.commit()
 		connection.close()
 		return render(request, "frontend/updateProfile.html")
